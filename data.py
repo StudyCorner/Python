1fc6bbcfce7665060e223b7693abf2e6e1230646
StudyCorner <131874444+StudyCorner@users.noreply.github.com>
2023-05-04 16:45:59 +0530

    Delete data.py

--- data.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    name = "aditi"
-    print("Name is ",name)
-    print("file name is ",__name__)
-else:
-    print("File got imported")
-    print("file name is ",__name__)
